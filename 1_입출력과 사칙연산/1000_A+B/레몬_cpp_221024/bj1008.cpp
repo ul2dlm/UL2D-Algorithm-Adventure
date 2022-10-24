ddcf9e78350f010233fe26104448d25d3d101fd8
ul2dlm <hyerim6733@naver.com>
2022-10-24 12:55:39 +0900

    1024_1

---
+++ 1_입출력과 사칙연산/1000_A+B/레몬_cpp_221024/bj1008.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double N, M;
+    if(M > 0) {
+        cin >> N >> M;
+        cout << N/M;
+    }
+    return 0;
+}
---
+++ 1_입출력과 사칙연산/1000_A+B/레몬_cpp_221024/bj10998.cpp
@@ -0,0 +1,16 @@
+/*
+    AxB
+    https://www.acmicpc.net/problem/10998
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double N, M;
+    cin >> N >> M;
+    cout << N*M;
+    
+    return 0;
+}
