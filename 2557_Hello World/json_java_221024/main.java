f8b992b39244a98f52e5393be92abf8bdfc4cc21
B560M-MORTAR\User <whlee@wkit.co.kr>
2022-10-24 10:54:27 +0900

    .

---
+++ 2557_Hello World/json_java_221024/main.java
@@ -0,0 +1,11 @@
+/*
+Hello World
+https://www.acmicpc.net/problem/2557
+*/
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
