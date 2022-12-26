4302d77aa10a4c6b57929dec1627e40a44a81190
ul2dlm <116530039+ul2dlm@users.noreply.github.com>
2022-12-26 17:46:09 +0900

    Update 벽부수고이동하기.py

--- 벽부수고이동하기.py
+++ 벽부수고이동하기.py
@@ -1,6 +1,47 @@
 import sys
 N, M = map(int, sys.stdin.readline().split())
 
-graph = [[0]*(M+1) for _ in range(N+1)]
+MAX = 1000001
+graph = []
+visited = [[MAX]*M for _ in range(N)]
+direct = [[0,1],[0,-1], [1,0], [-1,0]]
 
-print(graph)
+def bfs():
+    wall = True
+    que = []
+    que.append([0,0])
+    visited[0][0] = 1
+    
+    while que:
+        pos = que.pop()
+        x = pos[0]
+        y = pos[1]
+        for i in range(4):
+            nx = x + direct[i][0]
+            ny = y + direct[i][1]
+            if (nx >= 0 and nx < N and ny >= 0 and ny < M):
+                if graph[x][y] == '2':
+                    graph[x][y] = '0'
+                    wall = True
+                #print(nx,ny)
+                #print(graph[nx][ny] == '0', graph[nx][ny] == '1', visited[nx][ny] == 0 , wall == True)
+                if graph[nx][ny] == '0' and visited[nx][ny] == MAX :
+                    visited[nx][ny] = min(visited[x][y] + 1, visited[nx][ny])
+                    que.append([nx,ny])
+                elif graph[nx][ny] == '1' and visited[nx][ny] == MAX and wall == True:
+                    wall = False
+                    visited[nx][ny] = min(visited[x][y] + 1, visited[nx][ny])
+                    graph[x][y] = '2' #벽깬위치 기억
+                    que.append([nx,ny])
+                    
+
+for _ in range(N):
+    #x = sys.stdin.readline().split()
+    lst = list(sys.stdin.readline())
+    graph.append(lst)
+
+bfs()
+if visited[N-1][M-1] == MAX:
+    visited[N-1][M-1] = -1
+print(visited[N-1][M-1])
+
